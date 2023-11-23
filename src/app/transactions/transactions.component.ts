bf3706ada8a3adf6ee9273f084b178a186f47169
Lucila Alochis <lucila.alochis1999@gmail.com>
2023-11-23 15:09:37 -0300

    datos de transacciones y solicitud de préstamos

--- src/app/transactions/transactions.component.ts
+++ src/app/transactions/transactions.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Client } from '../Model/client';
+import { ClientService } from '../Service/client.service';
 
 @Component({
   selector: 'app-transactions',
   templateUrl: './transactions.component.html',
   styleUrls: ['./transactions.component.css']
 })
-export class TransactionsComponent {
+export class TransactionsComponent implements OnInit{
+  client: Client = new Client()
+  constructor(private clientService: ClientService) {}
+
+  ngOnInit(): void {
+    this.clientService.getCurrentClient().subscribe(
+      (data: Client) => {
+        this.client = data;
+        console.log(this.client);
+      },
+      (error) => {
+        console.error(error);
+      }
+    );
+  }
 
 }
