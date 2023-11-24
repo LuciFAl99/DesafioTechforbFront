58873aea9812eb2412e1251b46814c56b4111fff
Lucila Alochis <lucila.alochis1999@gmail.com>
2023-11-24 17:28:52 -0300

    form prestamos

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Menu } from '../Model/menu';
 import { MenuService } from '../Service/menu.service';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-sidebar',
@@ -9,13 +10,13 @@ import { MenuService } from '../Service/menu.service';
 })
 export class SidebarComponent implements OnInit{
 menu: Menu = {};
-constructor(private menuService: MenuService){}
+constructor(private menuService: MenuService, private http: HttpClient){}
 
 ngOnInit(): void {
   this.menuService.getMenu().subscribe(
     (data: Menu[]) => {
       if (data && data.length > 0) {
-        this.menu = data[0]; // Accede al primer objeto del array
+        this.menu = data[0];
         console.log(this.menu);
       }
     },
@@ -23,6 +24,10 @@ ngOnInit(): void {
       console.error(error);
     }
   );
-
+}
+logout() {
+  this.http.post('http://localhost:8080/api/logout', {}).subscribe(() => {
+    window.location.href = '/login';
+  });
 }
 }
